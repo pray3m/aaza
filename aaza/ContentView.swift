45bd3d35f22b03207a746a0e9f4abfe050b0cc54
pray3m <prem.gtm9@gmail.com>
2026-02-07 23:15:37 +0545

    Initial Commit

---
+++ aaza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aaza
+//
+//  Created by Prem Gautam on 7/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aaza/aazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aazaApp.swift
+//  aaza
+//
+//  Created by Prem Gautam on 7/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct aazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
